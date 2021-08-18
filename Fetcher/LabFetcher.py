aa5c49e196b76d513fcb347d8cc73c62743096e6
Chun-Sheng, Li <peter279k@gmail.com>
2021-08-18 22:08:09 +0800

    Using big5-hkscs for remote CSV file

--- Fetcher/LabFetcher.py
+++ Fetcher/LabFetcher.py
@@ -20,7 +20,7 @@ json_keys = [
 
 def fetch_labs():
     response = requests.get(lab_url)
-    response.encoding = 'utf-8'
+    response.encoding = 'big5-hkscs'
     contents = response.text.split('\n')[1:-1]
     counter = 0
 
