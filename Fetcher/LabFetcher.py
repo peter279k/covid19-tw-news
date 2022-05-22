c4ea56589eb42bbc72586f61cbbc064241d81cca
Peter <peter279k@gmail.com>
2022-05-22 17:06:06 +0800

    Fix lab data format and update Lab fetcher

--- Fetcher/LabFetcher.py
+++ Fetcher/LabFetcher.py
@@ -23,12 +23,34 @@ def fetch_labs():
     response.encoding = 'big5-hkscs'
     contents = response.text.split('\n')[1:-1]
     counter = 0
+    missed = ''
+    missed_index = 1
 
     for content in contents:
         line = list(csv.reader([content]))[0]
         if len(line) < 9:
-            for value in list(range(1, 9-len(line)+1)):
-                line.append('N/A')
+            missed += ','.join(line)
+            if missed_index % 2 == 0:
+                missed += '\n'
+            missed_index += 1
+            continue
+        lab_json[str(counter)] = {
+            'gency_code': line[0],
+            'area': line[1],
+            'county': line[2],
+            'admin_district': line[3],
+            'agency_name': line[4],
+            'address': line[5],
+            'phone_number': line[6],
+            'longitude': line[7],
+            'latitude': line[8],
+        }
+        counter += 1
+
+    for missed_str in missed.split('\n'):
+        line = list(csv.reader([missed_str]))[0]
+        if len(line) == 0:
+            continue
         lab_json[str(counter)] = {
             'gency_code': line[0],
             'area': line[1],
